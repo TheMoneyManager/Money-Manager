5111bde884c996a066a01a801db7ccf6983ca5ac
velirean <47877809+velirean@users.noreply.github.com>
2021-03-15 15:07:02 -0500

    Creación de tabla Expenses y correción a rollback de users
    Se creó el archivo de migración y el modelo para Expenses. También se corrigió el archivo de mgración que actualizaba la tabla users porque no tenía el nombre correcto de la columna a eliminar.

---
+++ database/migrations/2021_03_15_195823_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('account_id');
+            $table->float('amount');
+            $table->text('description');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}

51ee0eda1fd89636594736928bc25ce03e9ae22f
Ivan Morales <ivann_mg@hotmail.com>
2021-05-03 22:30:47 -0500

    Update shadow box to ultimos gastos container

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -15,48 +15,54 @@
     </div>
 
     <div class="grid grid-cols-3 gap-6">
-        <div class="col-span-2 divide-y divide-gray-300">
-            <p class="text-3xl uppercase text-gray-400 text-center py-4">Últimos gastos</p>
-            <div class="py-10">
-                <table class="table-fixed">
-                    <thead>
-                    <tr class="bg-gray-100 h-16">
-                        <th class="w-2/12">FECHA</th>
-                        <th class="w-2/12">CUENTA DE ORIGEN</th>
-                        <th class="w-2/12">CATEGORÍAS</th>
-                        <th class="w-2/12">MONTO</th>
-                        <th class="w-4/12">DESCRIPCIÓN</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($expenses as $expense)
-                            <tr>
-                                <td>{{ date_format($expense->created_at, "d/m/y") }}</td>
-                                <td>{{ $expense->account->name }}</td>
-                                <td>
-                                    @foreach ($expense->categories as $category)
-                                    {{ $category->name }}
-                                    @endforeach
-                                </td>
-                                <td>{{ $expense->amount }}</td>
-                                <td>{{ $expense->description }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <div class="text-center py-10">
-                <button type="submit" class="px-3 py-4 text-white bg-green-600 focus:bg-green-500 focus:outline-none">
-                    <a href="{{ route('expenses.create')}}" class="text-sm">
-                        REGISTRAR GASTO
-                    </a>
-                </button>
-            </div>
-        </div>
-        <div class="col-span-1 divide-y divide-gray-300 ">
+        <div class="col-span-2">
             <div class="shadow-md">
                 <div class="divide-y divide-gray-300">
-                    <div class="">
+                    <div>
+                        <p class="text-2xl text-gray-400 text-center py-4">Últimos gastos</p>
+                    </div>
+                    <div class="py-10">
+                        <table class="table-fixed">
+                            <thead>
+                            <tr class="bg-gray-100 h-16">
+                                <th class="w-2/12">FECHA</th>
+                                <th class="w-2/12">CUENTA DE ORIGEN</th>
+                                <th class="w-2/12">CATEGORÍAS</th>
+                                <th class="w-2/12">MONTO</th>
+                                <th class="w-4/12">DESCRIPCIÓN</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($expenses as $expense)
+                                    <tr>
+                                        <td>{{ date_format($expense->created_at, "d/m/y") }}</td>
+                                        <td>{{ $expense->account->name }}</td>
+                                        <td>
+                                            @foreach ($expense->categories as $category)
+                                            {{ $category->name }}
+                                            @endforeach
+                                        </td>
+                                        <td>{{ $expense->amount }}</td>
+                                        <td>{{ $expense->description }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="text-center py-10">
+                        <button type="submit" class="px-3 py-4 text-white bg-green-600 focus:bg-green-500 focus:outline-none">
+                            <a href="{{ route('expenses.create')}}" class="text-sm">
+                                Registar gasto
+                            </a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-1">
+            <div class="shadow-md">
+                <div class="divide-y divide-gray-300">
+                    <div>
                         <p class="text-2xl text-gray-400 ml-10 py-4">Cuentas de banco y tarjetas</p>
                     </div>
                     <div class="text-center py-2">

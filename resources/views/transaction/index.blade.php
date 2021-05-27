bd35a73eff1d00436c411a1d451edc67181e89d6
NavarroDGerardo <dgnvrrg@gmail.com>
2021-05-27 17:51:00 -0500

    index transacciones

---
+++ resources/views/transaction/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.sidebar')
+
+@section('content')
+
+<div class="container mx-auto px-4 py-4">
+    <div class="w-10/12 h-20 mx-auto border-b-2 ">
+        <h1 class="text-gray-600 font-bold text-4xl pt-5">TRANSACCIONES</h1>
+    </div>
+
+    <div class="w-8/12 h-4/6 mx-auto mt-6">
+        <table class="w-full border-gray-700 border-2">
+            <thead>
+                <tr class="table-fixed bg-gray-200 border-gray-700 border-2">
+                    <th class="pt-2 pb-2 font-medium border-gray-700 border-2">ID CUENTA ORIGEN</th>
+                    <th class="pt-2 pb-2 font-medium border-gray-700 border-2">NOMBRE CUENTA ORIGEN</th>
+                    <th class="pt-2 pb-2 font-medium border-gray-700 border-2">NOMBRE CUENTA DESTINO</th>
+                    <th class="pt-2 pb-2 font-medium border-gray-700 border-2">CANTIDAD</th>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                @foreach ($transactions as $transaction)
+                    <tr class="border border-gray-300">
+                        <th class="font-normal pt-4 pb-4 w-2/12 border-gray-700 border-2">{{ $transaction->id_CuentaOrigen}}</th>
+                        <th class="font-normal pt-4 pb-4 w-4/12 border-gray-700 border-2">{{ $accounts[$transaction->id]['name'] }}</th>
+                        <th class="font-normal pt-4 pb-4 w-4/12 border-gray-700 border-2">{{ $transaction->addresee }}</th>
+                        <th class="font-normal pt-4 pb-4 w-4/12 border-gray-700 border-2 text-red-600">- ${{number_format($transaction->amount, 2)}}</th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+            window.Echo.channel('ExpensesChannel').listen('NewExpenseNotification', (e) => {
+                console.log(e);
+                if(e.account.user_id == {{auth()->user()->id}}){
+                    $('#mensajeNoti').text("se hizo un gasto de " + e.expense.amount + "$ en tu cuenta " + e.account.name);
+                    document.getElementById('notification').style.visibility="visible";
+                    setTimeout(function() {
+                        document.getElementById('notification').style.visibility="hidden";
+                    }, 5000);
+                }else{
+                    e.users.forEach(user => {
+                        if(user.id == {{auth()->user()->id}}){
+                            $('#mensajeNoti').text("se hizo un gasto de " + e.expense.amount + "$ en tu cuenta COMPARTIDA " + e.account.name);
+                            document.getElementById('notification').style.visibility="visible";
+                            setTimeout(function() {
+                                document.getElementById('notification').style.visibility="hidden";
+                            }, 5000);
+                        }
+                    });
+                }
+            });
+        });
+</script>
+
+@endsection

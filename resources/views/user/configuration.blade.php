5094a843b56763f3ad7e406dc857f5d054ffb79e
AlanMendoza09 <37988489+AlanMendoza09@users.noreply.github.com>
2021-05-27 19:38:39 -0500

    Estilo configuracion arreglado

--- resources/views/user/configuration.blade.php
+++ resources/views/user/configuration.blade.php
@@ -15,19 +15,19 @@
             <form action="{{ route('user.update', ['user' => $user])}}" method="POST">
                 @csrf
                 @method('PUT')
-                <h3 class="text-gray-600 inline-block w-2/12 mt-10">nombre</h3>
+                <h3 class="text-gray-600 inline-block w-1/12 mt-10">nombre</h3>
                 <input type="text" value="{{$user->name}}" placeholder="John Doe" name="nombre" class="rounded-lg bg-gray-100 border-0 inline-block w-3/12 mt-10">
 
                 <h3 class="text-gray-600 inline-block ml-36 w-2/12 mt-10">Nueva Contraseña</h3>
                 <input type="password" placeholder="●●●●●●●" name="nuevaContrasenia" class="rounded-lg bg-gray-100 border-0 inline-block w-3/12 mt-10">
 
-                <h3 class="text-gray-600 inline-block w-2/12 mt-10">E-mail</h3>
+                <h3 class="text-gray-600 inline-block w-1/12 mt-10">E-mail</h3>
                 <input type="email" value="{{$user->email}}" placeholder="error@mail.com" name="email" class="rounded-lg bg-gray-100 border-0 inline-block w-3/12 mt-10">
 
                 <h3 class="text-gray-600 inline-block ml-36 w-2/12 mt-10">Confirmar Contraseña</h3>
                 <input type="password" placeholder="●●●●●●●" name="confirmacionContrasenia" class="rounded-lg bg-gray-100 border-0 inline-block w-3/12 mt-10">
 
-                <button type="submit" class="px-3 py-4 text-white bg-green-500 rounded-md focus:bg-green-600 focus:outline-none hover:bg-green-600 cursor-pointer">Guardar cambios</button>
+                <button type="submit" class="px-3 py-4 text-white bg-green-500 rounded-md focus:bg-green-600 focus:outline-none hover:bg-green-600 cursor-pointer mt-10">Guardar cambios</button>
             </form>
         </div>
     </div>
